5e18a2daae0cde2b9260c574317d8e360a74709e
Olivier Bellone <ob@stripe.com>
2018-02-25 14:34:24 +0100

    Remove deprecated code

--- tests/test_error.py
+++ tests/test_error.py
@@ -2,24 +2,24 @@
 
 from __future__ import absolute_import, division, print_function
 
-from stripe import six, StripeError
+from stripe import six, error
 
 
 class TestError(object):
     def test_formatting(self):
-        err = StripeError(u'öre')
+        err = error.StripeError(u'öre')
         assert six.text_type(err) == u'öre'
         if six.PY2:
             assert str(err) == '\xc3\xb6re'
 
     def test_formatting_with_request_id(self):
-        err = StripeError(u'öre', headers={'request-id': '123'})
+        err = error.StripeError(u'öre', headers={'request-id': '123'})
         assert six.text_type(err) == u'Request 123: öre'
         if six.PY2:
             assert str(err) == 'Request 123: \xc3\xb6re'
 
     def test_formatting_with_none(self):
-        err = StripeError(None, headers={'request-id': '123'})
+        err = error.StripeError(None, headers={'request-id': '123'})
         assert six.text_type(err) == u'Request 123: <empty message>'
         if six.PY2:
             assert str(err) == 'Request 123: <empty message>'
